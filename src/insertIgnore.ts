25e605ceffe499cf3a77aec8f83d58130a0f42eb
Aditya Thakral <aditya.thakral@data.world>
2019-08-05 20:41:02 -0500

    :sparkles: add context to ts-ignores

--- src/insertIgnore.ts
+++ src/insertIgnore.ts
@@ -1,8 +1,8 @@
-import ts from "typescript";
-import * as utils from "tsutils";
-import { NodeWrap } from "tsutils";
+import ts from 'typescript';
+import * as utils from 'tsutils';
+import { NodeWrap } from 'tsutils';
 
-const IGNORE_TEXT = "// @ts-ignore FIXME";
+const IGNORE_TEXT = '// @ts-ignore';
 
 // JsxElement = 260,
 // JsxSelfClosingElement = 261,
@@ -33,6 +33,32 @@ function getLine(diagnostic: ts.Diagnostic, position?: number) {
   return line;
 }
 
+function specificIgnoreText(diagnostic: ts.Diagnostic) {
+  const message = ts.flattenDiagnosticMessageText(diagnostic.messageText, ';');
+
+  const missingTypes = message.match(
+    /^Could not find a declaration file for module '(([a-z]|[A-Z]|\-|\.|\@|\/)*)'/
+  );
+  if (missingTypes) {
+    const packageName = missingTypes[1];
+    return `Missing "@types/${packageName}"`;
+  }
+
+  if (message.endsWith(' has no default export.')) {
+    return `Use "import * as Foo from 'foo'" syntax if 'foo' does not export a default value.`;
+  }
+
+  return message;
+}
+
+function ignoreText(diagnostic: ts.Diagnostic) {
+  const specificText = specificIgnoreText(diagnostic);
+
+  return specificText == null
+    ? IGNORE_TEXT
+    : `${IGNORE_TEXT} -- ${specificText}`;
+}
+
 export default function insertIgnore(
   diagnostic: ts.Diagnostic,
   codeSplitByLine: string[],
@@ -51,7 +77,7 @@ export default function insertIgnore(
     return codeSplitByLine;
   }
 
-  codeSplitByLine.splice(line, 0, IGNORE_TEXT);
+  codeSplitByLine.splice(line, 0, ignoreText(diagnostic));
 
   return codeSplitByLine;
 }
