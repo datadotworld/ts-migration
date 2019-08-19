e0da5627876b2422f4ff6c6acd6885c9e8bd8d9a
Adam McElwee <adam.mcelwee@data.world>
2019-08-19 16:50:56 -0500

    :package: Support optional --project flag + add missing commander dependency (#13)

--- dist/cli.js
+++ dist/cli.js
@@ -10,26 +10,30 @@ const ignoreErrorsRunner_1 = __importDefault(require("./ignoreErrorsRunner"));
 const ignoreFileErrorsRunner_1 = __importDefault(require("./ignoreFileErrorsRunner"));
 const convertCodebase_1 = __importDefault(require("./convertCodebase"));
 const checkRunner_1 = __importDefault(require("./checkRunner"));
-const rootDir = process.cwd();
-const { configJSON } = tsCompilerHelpers_1.createTSCompiler(rootDir);
-const filePaths = {
-    rootDir,
-    include: configJSON.config.include,
-    exclude: configJSON.config.exclude,
-    extensions: [".ts", ".tsx"]
+const constructPaths = (rootDir = process.cwd()) => {
+    const { configJSON } = tsCompilerHelpers_1.createTSCompiler(rootDir);
+    return {
+        rootDir,
+        include: configJSON.config.include,
+        exclude: configJSON.config.exclude,
+        extensions: [".ts", ".tsx"]
+    };
 };
 commander_1.default
     .command("strip-comments")
+    .option("--project <path>")
     .option("-c, --commit")
     .option("--comments <list>", "A comma-seperated list of comments to strip. Must start with `//`", (f) => f.split(","))
     .action((cmd) => {
     console.log("Stripping comments from files...");
+    const filePaths = constructPaths(cmd.project);
     if (cmd.comments)
         console.log("Removing comments: ", cmd.comments);
     stripCommentsRunner_1.default(filePaths, cmd.comments, !!cmd.commit);
 });
 commander_1.default
     .command("convert-codebase")
+    .option("--project <path>")
     .option("-c, --commit")
     .option("--rename")
     // TODO support directory?
@@ -37,18 +41,21 @@ commander_1.default
     .option("--exclude <list>", "A comma-seperated list of strings to exclude", (f) => f.split(","))
     .action((cmd) => {
     console.log("Converting the codebase from Flow to Typescript");
+    const filePaths = constructPaths(cmd.project);
     const paths = Object.assign({}, filePaths, { exclude: [...filePaths.exclude, ...(cmd.exclude || [])], extensions: [".js", ".jsx"] });
     console.log(paths);
     convertCodebase_1.default(paths, !!cmd.commit, !!cmd.rename, cmd.files);
 });
 commander_1.default
     .command("ignore-errors")
+    .option("--project <path>")
     .option("-c, --commit")
     .option("--includeJSX", "Insert ignores into JSX -- may cause runtime changes!", true)
     .option("--exclude <list>", "A comma-seperated list of strings to exclude", (f) => f.split(","))
     .option("--files <list>", "A comma-seperated list of files to convert", (f) => f.split(","))
     .action((cmd) => {
     console.log("Ignoring Typescript errors...");
+    const filePaths = constructPaths(cmd.project);
     const paths = cmd.files && cmd.files.length > 0
         ? Object.assign({}, filePaths, { include: [...cmd.files], exclude: [] }) : Object.assign({}, filePaths, { exclude: [...filePaths.exclude, ...(cmd.exclude || [])] });
     console.log(paths);
@@ -56,16 +63,20 @@ commander_1.default
 });
 commander_1.default
     .command("ignore-file-errors")
+    .option("--project <path>")
     .option("-c, --commit")
     .action((cmd) => {
     console.log("Inserting custom ts-ignore pragmas...");
+    const filePaths = constructPaths(cmd.project);
     ignoreFileErrorsRunner_1.default(filePaths, !!cmd.commit);
 });
 commander_1.default
     .command("check-types")
+    .option("--project <path>")
     .option("-c, --commit")
-    .action(() => {
+    .action((cmd) => {
     console.log("Checking Typescript types and skipping ignored files...");
+    const filePaths = constructPaths(cmd.project);
     checkRunner_1.default(filePaths);
 });
 commander_1.default.parse(process.argv);
--- src/cli.ts
+++ src/cli.ts
@@ -6,10 +6,6 @@ import ignoreFileErrors from "./ignoreFileErrorsRunner";
 import convertCodebase from "./convertCodebase";
 import checkTypes from "./checkRunner";
 
-const rootDir = process.cwd();
-
-const { configJSON } = createTSCompiler(rootDir);
-
 export interface FilePaths {
   rootDir: string;
   include: string[];
@@ -17,15 +13,19 @@ export interface FilePaths {
   extensions: string[];
 }
 
-const filePaths: FilePaths = {
-  rootDir,
-  include: configJSON.config.include,
-  exclude: configJSON.config.exclude,
-  extensions: [".ts", ".tsx"]
+const constructPaths = (rootDir: string = process.cwd()): FilePaths => {
+  const { configJSON } = createTSCompiler(rootDir);
+  return {
+    rootDir,
+    include: configJSON.config.include,
+    exclude: configJSON.config.exclude,
+    extensions: [".ts", ".tsx"]
+  };
 };
 
 program
   .command("strip-comments")
+  .option("--project <path>")
   .option("-c, --commit")
   .option(
     "--comments <list>",
@@ -33,8 +33,13 @@ program
     (f: string) => f.split(",")
   )
   .action(
-    (cmd: { commit: boolean | undefined; comments: string[] | undefined }) => {
+    (cmd: {
+      project?: string;
+      commit: boolean | undefined;
+      comments: string[] | undefined;
+    }) => {
       console.log("Stripping comments from files...");
+      const filePaths = constructPaths(cmd.project);
       if (cmd.comments) console.log("Removing comments: ", cmd.comments);
       stripComments(filePaths, cmd.comments, !!cmd.commit);
     }
@@ -42,6 +47,7 @@ program
 
 program
   .command("convert-codebase")
+  .option("--project <path>")
   .option("-c, --commit")
   .option("--rename")
   // TODO support directory?
@@ -57,12 +63,14 @@ program
   )
   .action(
     (cmd: {
+      project?: string;
       commit: boolean | undefined;
       rename: boolean | undefined;
       files: string[] | undefined;
       exclude: string[] | undefined;
     }) => {
       console.log("Converting the codebase from Flow to Typescript");
+      const filePaths = constructPaths(cmd.project);
       const paths = {
         ...filePaths,
         exclude: [...filePaths.exclude, ...(cmd.exclude || [])],
@@ -75,6 +83,7 @@ program
 
 program
   .command("ignore-errors")
+  .option("--project <path>")
   .option("-c, --commit")
   .option(
     "--includeJSX",
@@ -93,12 +102,14 @@ program
   )
   .action(
     (cmd: {
+      project?: string;
       commit: boolean | undefined;
       exclude: string[] | undefined;
       includeJSX: boolean;
       files?: string[];
     }) => {
       console.log("Ignoring Typescript errors...");
+      const filePaths = constructPaths(cmd.project);
       const paths =
         cmd.files && cmd.files.length > 0
           ? {
@@ -118,18 +129,21 @@ program
 
 program
   .command("ignore-file-errors")
+  .option("--project <path>")
   .option("-c, --commit")
-  .action((cmd: { commit: boolean | undefined }) => {
+  .action((cmd: { project?: string; commit: boolean | undefined }) => {
     console.log("Inserting custom ts-ignore pragmas...");
+    const filePaths = constructPaths(cmd.project);
     ignoreFileErrors(filePaths, !!cmd.commit);
   });
 
 program
   .command("check-types")
+  .option("--project <path>")
   .option("-c, --commit")
-  .action(() => {
+  .action((cmd: { project?: string }) => {
     console.log("Checking Typescript types and skipping ignored files...");
-
+    const filePaths = constructPaths(cmd.project);
     checkTypes(filePaths);
   });
 
