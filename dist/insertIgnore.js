c3be9e8f342c9c8c9b880a013374638e149933c4
chrisbodhi <chris.boette@data.world>
2020-06-10 11:29:38 -0500

    Update to TS 3.9.2.

--- dist/insertIgnore.js
+++ dist/insertIgnore.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.insertIgnore = exports.getMissingTypePackages = void 0;
 const typescript_1 = __importDefault(require("typescript"));
 // import * as utils from "tsutils";
 // import { NodeWrap } from "tsutils";
