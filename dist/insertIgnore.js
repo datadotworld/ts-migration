4e6186f4413a5472782ca0f83036711ccae92db2
chrisbodhi <chris.boette@data.world>
2020-06-10 13:20:32 -0500

    Uncomment parent jsx check. Build dist.

--- dist/insertIgnore.js
+++ dist/insertIgnore.js
@@ -1,12 +1,30 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.insertIgnore = exports.getMissingTypePackages = void 0;
 const typescript_1 = __importDefault(require("typescript"));
-// import * as utils from "tsutils";
-// import { NodeWrap } from "tsutils";
+const utils = __importStar(require("tsutils"));
 const IGNORE_TEXT = "// @ts-ignore";
 const missingTypesPackages = new Set();
 // JsxElement = 260,
@@ -20,18 +38,16 @@ const missingTypesPackages = new Set();
 // JsxAttributes = 268,
 // JsxSpreadAttribute = 269,
 // JsxExpression = 270,
-// function findParentJSX(n: NodeWrap | undefined): [number, NodeWrap] | null {
-//   if (n) {
-//     if (
-//       n.kind >= ts.SyntaxKind.JsxElement &&
-//       n.kind <= ts.SyntaxKind.JsxExpression
-//     ) {
-//       return [n.kind, n];
-//     }
-//     return findParentJSX(n.parent);
-//   }
-//   return null;
-// }
+function findParentJSX(n) {
+    if (n) {
+        if (n.kind >= typescript_1.default.SyntaxKind.JsxElement &&
+            n.kind <= typescript_1.default.SyntaxKind.JsxExpression) {
+            return [n.kind, n];
+        }
+        return findParentJSX(n.parent);
+    }
+    return null;
+}
 function getLine(diagnostic, position) {
     const { line } = diagnostic.file.getLineAndCharacterOfPosition(position || diagnostic.start);
     return line;
@@ -50,9 +66,9 @@ function specificIgnoreText(diagnostic) {
     }
     return `${message}${code}`;
 }
-// function nodeContainsTSIgnore(node: ts.Node): boolean {
-//   return ts.isJsxText(node) && node.text.includes(IGNORE_TEXT);
-// }
+function nodeContainsTSIgnore(node) {
+    return typescript_1.default.isJsxText(node) && node.text.includes(IGNORE_TEXT);
+}
 function ignoreText(diagnostic, rootDir) {
     const specificText = specificIgnoreText(diagnostic);
     return specificText == null
@@ -64,39 +80,32 @@ function getMissingTypePackages() {
 }
 exports.getMissingTypePackages = getMissingTypePackages;
 function insertIgnore(diagnostic, codeSplitByLine, includeJSX, rootDir) {
-    // const convertedAST = utils.convertAst(diagnostic.file!);
-    // const n = utils.getWrappedNodeAtPosition(
-    //   convertedAST.wrapped,
-    //   diagnostic.start!
-    // );
+    const convertedAST = utils.convertAst(diagnostic.file);
+    const n = utils.getWrappedNodeAtPosition(convertedAST.wrapped, diagnostic.start);
     const line = getLine(diagnostic);
-    // const isInJSX = findParentJSX(n);
+    const isInJSX = findParentJSX(n);
     if (!includeJSX) {
         // Don't add ignores in JSX since it's too hard.
         return codeSplitByLine;
     }
     const ignoreComment = ignoreText(diagnostic, rootDir);
-    // const maybeResult = [
-    //   ...codeSplitByLine.slice(0, line),
-    //   IGNORE_TEXT,
-    //   ...codeSplitByLine.slice(line)
-    // ];
-    // if (isInJSX) {
-    //   const sourceFile = ts.createSourceFile(
-    //     diagnostic.file!.fileName,
-    //     maybeResult.join("\n"),
-    //     ts.ScriptTarget.ESNext
-    //   );
-    //   const newConvertedAst = utils.convertAst(sourceFile);
-    //   if (newConvertedAst.flat.some(nodeContainsTSIgnore)) {
-    //     return [
-    //       ...codeSplitByLine.slice(0, line),
-    //       "{ /*",
-    //       `${ignoreComment} */ }`,
-    //       ...codeSplitByLine.slice(line)
-    //     ];
-    //   }
-    // }
+    const maybeResult = [
+        ...codeSplitByLine.slice(0, line),
+        IGNORE_TEXT,
+        ...codeSplitByLine.slice(line)
+    ];
+    if (isInJSX) {
+        const sourceFile = typescript_1.default.createSourceFile(diagnostic.file.fileName, maybeResult.join("\n"), typescript_1.default.ScriptTarget.ESNext);
+        const newConvertedAst = utils.convertAst(sourceFile);
+        if (newConvertedAst.flat.some(nodeContainsTSIgnore)) {
+            return [
+                ...codeSplitByLine.slice(0, line),
+                "{ /*",
+                `${ignoreComment} */ }`,
+                ...codeSplitByLine.slice(line)
+            ];
+        }
+    }
     // Ensure proper sequencing of eslint ignores and ts-ignores
     if (codeSplitByLine.length > 0 &&
         line > 0 &&
--- src/insertIgnore.ts
+++ src/insertIgnore.ts
@@ -1,6 +1,6 @@
 import ts from "typescript";
-// import * as utils from "tsutils";
-// import { NodeWrap } from "tsutils";
+import * as utils from "tsutils";
+import { NodeWrap } from "tsutils";
 
 const IGNORE_TEXT = "// @ts-ignore";
 const missingTypesPackages = new Set<string>();
@@ -16,18 +16,18 @@ const missingTypesPackages = new Set<string>();
 // JsxAttributes = 268,
 // JsxSpreadAttribute = 269,
 // JsxExpression = 270,
-// function findParentJSX(n: NodeWrap | undefined): [number, NodeWrap] | null {
-//   if (n) {
-//     if (
-//       n.kind >= ts.SyntaxKind.JsxElement &&
-//       n.kind <= ts.SyntaxKind.JsxExpression
-//     ) {
-//       return [n.kind, n];
-//     }
-//     return findParentJSX(n.parent);
-//   }
-//   return null;
-// }
+function findParentJSX(n: NodeWrap | undefined): [number, NodeWrap] | null {
+  if (n) {
+    if (
+      n.kind >= ts.SyntaxKind.JsxElement &&
+      n.kind <= ts.SyntaxKind.JsxExpression
+    ) {
+      return [n.kind, n];
+    }
+    return findParentJSX(n.parent);
+  }
+  return null;
+}
 
 function getLine(diagnostic: ts.Diagnostic, position?: number) {
   const { line } = diagnostic!.file!.getLineAndCharacterOfPosition(
@@ -56,9 +56,9 @@ function specificIgnoreText(diagnostic: ts.Diagnostic) {
   return `${message}${code}`;
 }
 
-// function nodeContainsTSIgnore(node: ts.Node): boolean {
-//   return ts.isJsxText(node) && node.text.includes(IGNORE_TEXT);
-// }
+function nodeContainsTSIgnore(node: ts.Node): boolean {
+  return ts.isJsxText(node) && node.text.includes(IGNORE_TEXT);
+}
 
 function ignoreText(diagnostic: ts.Diagnostic, rootDir: string) {
   const specificText = specificIgnoreText(diagnostic);
@@ -78,43 +78,43 @@ export function insertIgnore(
   includeJSX: boolean,
   rootDir: string
 ) {
-  // const convertedAST = utils.convertAst(diagnostic.file!);
-  // const n = utils.getWrappedNodeAtPosition(
-  //   convertedAST.wrapped,
-  //   diagnostic.start!
-  // );
+  const convertedAST = utils.convertAst(diagnostic.file!);
+  const n = utils.getWrappedNodeAtPosition(
+    convertedAST.wrapped,
+    diagnostic.start!
+  );
   const line = getLine(diagnostic);
 
-  // const isInJSX = findParentJSX(n);
+  const isInJSX = findParentJSX(n);
   if (!includeJSX) {
     // Don't add ignores in JSX since it's too hard.
     return codeSplitByLine;
   }
 
   const ignoreComment = ignoreText(diagnostic, rootDir);
-  // const maybeResult = [
-  //   ...codeSplitByLine.slice(0, line),
-  //   IGNORE_TEXT,
-  //   ...codeSplitByLine.slice(line)
-  // ];
+  const maybeResult = [
+    ...codeSplitByLine.slice(0, line),
+    IGNORE_TEXT,
+    ...codeSplitByLine.slice(line)
+  ];
 
-  // if (isInJSX) {
-  //   const sourceFile = ts.createSourceFile(
-  //     diagnostic.file!.fileName,
-  //     maybeResult.join("\n"),
-  //     ts.ScriptTarget.ESNext
-  //   );
-  //   const newConvertedAst = utils.convertAst(sourceFile);
+  if (isInJSX) {
+    const sourceFile = ts.createSourceFile(
+      diagnostic.file!.fileName,
+      maybeResult.join("\n"),
+      ts.ScriptTarget.ESNext
+    );
+    const newConvertedAst = utils.convertAst(sourceFile);
 
-  //   if (newConvertedAst.flat.some(nodeContainsTSIgnore)) {
-  //     return [
-  //       ...codeSplitByLine.slice(0, line),
-  //       "{ /*",
-  //       `${ignoreComment} */ }`,
-  //       ...codeSplitByLine.slice(line)
-  //     ];
-  //   }
-  // }
+    if (newConvertedAst.flat.some(nodeContainsTSIgnore)) {
+      return [
+        ...codeSplitByLine.slice(0, line),
+        "{ /*",
+        `${ignoreComment} */ }`,
+        ...codeSplitByLine.slice(line)
+      ];
+    }
+  }
 
   // Ensure proper sequencing of eslint ignores and ts-ignores
   if (
